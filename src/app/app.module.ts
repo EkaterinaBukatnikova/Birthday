cea9af2d0b92952f07fdc19974fb74c7d7d02c5b
Ekaterina <kbukatnikova263@gmail.com>
2022-03-18 14:14:51 +0500

    Глава 6 HttpClient и отправка запросов.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
  
-import { ReactiveFormsModule }   from '@angular/forms';
+import { HttpClientModule }   from '@angular/common/http';
+ 
 @NgModule({
-    imports:      [ BrowserModule, FormsModule, ReactiveFormsModule],
+    imports:      [ BrowserModule, FormsModule, HttpClientModule],
     declarations: [ AppComponent],
     bootstrap:    [ AppComponent ]
 })
