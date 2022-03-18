ef1be736be9b9f59d544e73fa3bae0984c58e76c
Ekaterina <kbukatnikova263@gmail.com>
2022-03-18 10:12:56 +0500

    Глава 4. Один сервис для всех компонентов.

--- src/app/data.component.ts
+++ src/app/data.component.ts
@@ -2,27 +2,26 @@ import { Component} from '@angular/core';
 import {DataService} from './data.service';
         
 @Component({
-    selector: 'my-app',
-    template: `<div class="panel">
-        <div><input [(ngModel)]="name" placeholder = "Модель" />
-            <button (click)="addItem(name)">Добавить</button>
+    selector: 'data-comp',
+    template: `<div>
+        <div>
+                <input [(ngModel)]="newItem" placeholder = "Модель" />
+                <button (click)="addItem(newItem)">Добавить</button>
         </div>
         <table>
             <tr *ngFor="let item of items">
                 <td>{{item}}</td>
             </tr>
         </table>
-    </div>`,
-    providers: [DataService]
+    </div>`
 })
-export class AppComponent{ 
+export class DataComponent{ 
       
+    newItem: string = "";
     items: string[] = [];
-    name: string = "";
     constructor(private dataService: DataService){}
       
     addItem(name: string){
-          
         this.dataService.addData(name);
     }
     ngOnInit(){
