232b5982de256ca6ffc35ffaf18155d9d0e281c8
Ekaterina <kbukatnikova263@gmail.com>
2022-03-18 10:52:10 +0500

    Глава 5. Reactive Forms.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,39 +1,70 @@
 import { Component} from '@angular/core';
-import { NgForm} from '@angular/forms';
- 
+import { FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+  
 @Component({
     selector: 'my-app',
     styles: [`
         input.ng-touched.ng-invalid {border:solid red 2px;}
         input.ng-touched.ng-valid {border:solid green 2px;}
     `],
-    template: `<form #myForm="ngForm" novalidate (ngSubmit)="onSubmit(myForm)">
+    template: `<form [formGroup]="myForm" novalidate (ngSubmit)="submit()">
                     <div class="form-group">
                         <label>Имя</label>
-                        <input class="form-control" name="name" ngModel required />
+                        <input class="form-control" name="name" formControlName="userName" />
+                         
+                        <div class="alert alert-danger"
+                            *ngIf="myForm.controls['userName'].invalid && myForm.controls['userName'].touched">
+                            Не указано имя
+                        </div>
                     </div>
                     <div class="form-group">
                         <label>Email</label>
-                        <input class="form-control" name="email" ngModel 
-                            required email />
+                        <input class="form-control" name="email" formControlName="userEmail" />
+                         
+                        <div class="alert alert-danger"
+                            *ngIf="myForm.controls['userEmail'].invalid && myForm.controls['userEmail'].touched">
+                            Некорректный email
+                        </div>
+                    </div>
+                    <div formArrayName="phones">
+                        <div class="form-group" *ngFor="let phone of getFormsControls()['controls']; let i = index">
+                            <label>Телефон</label>
+                            <input class="form-control" formControlName="{{i}}" />
+                        </div>
                     </div>
                     <div class="form-group">
-                        <label>Телефон</label>
-                        <input class="form-control" name="phone" ngModel 
-                            required pattern="[0-9]{10}" />
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" [disabled]="myForm.invalid" class="btn btn-default" value="Отправить" />
+                        <button class="btn btn-default" (click)="addPhone()">
+                            Добавить телефон
+                        </button>
+                        <button class="btn btn-default" [disabled]="myForm.invalid">
+                            Отправить
+                        </button>
                     </div>
                 </form>`
 })
 export class AppComponent { 
- 
-    name: string = "";
-    email: string = "";
-    phone: string = "";
-     
-    onSubmit(form: NgForm){
-        console.log(form);
+  
+    myForm : FormGroup;
+    constructor(){
+        this.myForm = new FormGroup({
+             
+            "userName": new FormControl("Tom", [Validators.required]),
+            "userEmail": new FormControl("", [
+                                Validators.required, 
+                                Validators.email 
+                            ]),
+            "phones": new FormArray([
+                new FormControl("+7", Validators.required)
+            ])
+        });
+    }
+    getFormsControls() : FormArray{
+        return this.myForm.controls['phones'] as FormArray;
+    }
+    addPhone(){
+        (<FormArray>this.myForm.controls["phones"]).push(new FormControl("+7", Validators.required));
+    }
+    submit(){
+        console.log(this.myForm);
     }
 }
